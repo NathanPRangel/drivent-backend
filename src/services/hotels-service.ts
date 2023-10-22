b7d4f5f14fc218cade8a66e1df41d9a38167a5bb
NathanPRangel <nathanpk20@gmail.com>
2023-10-22 11:50:34 -0300

    ajustes  em informações retornadas pelo endpoint

--- src/services/hotels-service.ts
+++ src/services/hotels-service.ts
@@ -1,7 +1,14 @@
 import { TicketStatus } from '@prisma/client';
 import { invalidDataError, notFoundError } from '@/errors';
 import { cannotListHotelsError } from '@/errors/cannot-list-hotels-error';
-import { enrollmentRepository, hotelRepository, ticketsRepository } from '@/repositories';
+import {
+  bookingRepository,
+  enrollmentRepository,
+  hotelRepository,
+  roomRepository,
+  ticketsRepository,
+} from '@/repositories';
+import { HotelWithDetails } from '@/protocols';
 
 async function validateUserBooking(userId: number) {
   const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
@@ -17,13 +24,79 @@ async function validateUserBooking(userId: number) {
   }
 }
 
+async function accomodationsTypeHelper(hotelId: number) {
+  const accomodations = await roomRepository.findHotelAccommodations(hotelId);
+
+  if (accomodations.length === 0) return 'Indisponível no momento!';
+  if (accomodations.length === 3) return 'Single, Double e Triple';
+
+  let text = '';
+  let count = 0;
+
+  accomodations.forEach((current) => {
+    switch (current.capacity) {
+      case 1:
+        text += 'Single';
+        count++;
+        break;
+      case 2:
+        if (count + 1 === accomodations.length) {
+          text += ' e ';
+        } else if (count === 1) {
+          text += ', ';
+        }
+
+        text += 'Double';
+        count++;
+        break;
+      case 3:
+        if (count > 0) text += ' e ';
+        text += 'Triple';
+        count++;
+        break;
+      default:
+        text = 'Indisponível no momento!';
+        break;
+    }
+  });
+
+  return text;
+}
+
 async function getHotels(userId: number) {
   await validateUserBooking(userId);
 
   const hotels = await hotelRepository.findHotels();
   if (hotels.length === 0) throw notFoundError();
 
-  return hotels;
+  const hotelsWithDetails: HotelWithDetails[] = [];
+
+  for (let i = 0; i < hotels.length; i++) {
+    const accommodations = await accomodationsTypeHelper(hotels[i].id);
+    console.log(accommodations);
+
+    const {
+      _sum: { capacity },
+    } = await roomRepository.findHotelHotelTotalCapacity(hotels[i].id);
+    console.log('full capacity', capacity);
+
+    const rooms = await roomRepository.findAllByHotelId(hotels[i].id);
+    const hotelRoomsId = rooms.map((room) => room.id);
+    console.log('hotelRoomsId', hotelRoomsId);
+
+    const bookings = await bookingRepository.findByRoomId(hotelRoomsId);
+    console.log('bookings', bookings);
+
+    const capacityAvailable = capacity - bookings.length;
+
+    hotelsWithDetails.push({
+      ...hotels[i],
+      accommodations,
+      capacityAvailable: capacityAvailable === -1 ? 0 : capacityAvailable,
+    });
+  }
+
+  return hotelsWithDetails;
 }
 
 async function getHotelsWithRooms(userId: number, hotelId: number) {
